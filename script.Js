16a804a960ceb0be78d9c6a79cf751ea6b438470
mallamhassan <hassanadeleke07@gmail.com>
2022-08-10 10:05:41 +0100

    feat:class exercise

---
+++ script.Js
@@ -0,0 +1,18 @@
+let hassan = "The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.";
+console.log(hassan);
+let words="Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.";
+console.log(words);
+let string='10';
+let number=10;
+console.log(string == number);
+
+
+let kenny="jargon and phyton"
+console.log(kenny.includes('on'));
+let sentence="I hope this course is not full of jargon";
+console.log(sentence.includes("jargon"))
+
+
+
+let because= "You cannot end a sentence with because because because is a conjunction";
+console.log(because.substr(31,23));
